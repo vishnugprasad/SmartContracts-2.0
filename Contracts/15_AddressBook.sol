1cb877cd6799e6107c12e7d6ab9540ed6b6fb726
vishnugprasad <vishnuanugraha001@gmail.com>
2024-03-06 21:12:24 +0530

    addressbook

---
+++ Contracts/15_AddressBook.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract ContactBook {
+
+    struct Contact {
+        string name;
+        string phoneNumber;
+        string emailId;
+    }
+
+    mapping(string => Contact) private contacts;
+
+    function setContact(string memory _name, string memory _phoneNumber, string memory _emailId) public {
+        contacts[_name] = Contact(_name, _phoneNumber, _emailId);
+
+    }
+
+    function getContact(string memory _name) public view returns(string memory name, string memory phoneNumber, string memory emailId){
+        require(bytes(contacts[_name].name).length != 0, "contact dosent exists");
+        Contact memory contact = contacts[_name];
+        return(contact.name, contact.phoneNumber, contact.emailId);
+    }
+
+    function removeContact(string memory _name) public {
+        require(bytes(contacts[_name].name).length !=0, "Contact dosent exist");
+        delete contacts[_name];
+    }
+}
